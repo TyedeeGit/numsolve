08aa5df664255d1cb3b70182ae7a095a702207e2
TyedeeGit <79290235+TyedeeGit@users.noreply.github.com>
2024-04-07 21:23:20 -0700

    Added polynomial solving to all_games.py.

--- all_games.py
+++ all_games.py
@@ -1,7 +1,9 @@
 from typing import Type
 from game import Game
 from prime_factors import PrimeFactors
+from polynomial_solving import PolynomialSolving
 
 ALL_GAMES: tuple[Type[Game], ...] = (
     PrimeFactors,
+    PolynomialSolving
 )
