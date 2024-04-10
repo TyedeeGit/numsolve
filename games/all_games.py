78433c692aa081f271883939edcba91f2cc7f7d6
TyedeeGit <79290235+TyedeeGit@users.noreply.github.com>
2024-04-10 16:26:35 -0700

    Added Convergence to all games.

--- games/all_games.py
+++ games/all_games.py
@@ -2,8 +2,10 @@ from typing import Type
 from gamelib.game import Game
 from .prime_factors.prime_factors import PrimeFactors
 from .polynomial_solving.polynomial_solving import PolynomialSolving
+from .convergence.convergence import Convergence
 
 ALL_GAMES: tuple[Type[Game], ...] = (
     PrimeFactors,
-    PolynomialSolving
+    PolynomialSolving,
+    Convergence
 )
