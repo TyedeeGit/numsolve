1d88950c11d24a37b88362c5af88c3760dac3124
TyedeeGit <79290235+TyedeeGit@users.noreply.github.com>
2024-04-11 08:08:14 -0700

    Removed Convergence from games due to being incomplete.

--- games/all_games.py
+++ games/all_games.py
@@ -2,10 +2,8 @@ from typing import Type
 from gamelib.game import Game
 from .prime_factors.prime_factors import PrimeFactors
 from .polysolve.polysolve import Polysolve
-from .convergence.convergence import Convergence
 
 ALL_GAMES: tuple[Type[Game], ...] = (
     PrimeFactors,
     Polysolve,
-    Convergence
 )
