5ba69982e196d20dd4ff79363e7e8be6c6057755
TyedeeGit <79290235+TyedeeGit@users.noreply.github.com>
2024-04-12 12:10:20 -0700

    Moved all gamelib modules into folders.

--- numsolve/main.py
+++ numsolve/main.py
@@ -19,6 +19,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+import fixpath
 from math import prod, sqrt, floor
 from decimal import Decimal
 from typing import Optional
